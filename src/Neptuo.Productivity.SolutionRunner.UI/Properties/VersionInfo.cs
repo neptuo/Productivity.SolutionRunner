7d59d64d97b1978ffb5b19696ce9222c09088a00
maraf <mara@neptuo.com>
2016-06-25 15:15:04 +0200

    Update version info.

--- src/Neptuo.Productivity.SolutionRunner.UI/Properties/VersionInfo.cs
+++ src/Neptuo.Productivity.SolutionRunner.UI/Properties/VersionInfo.cs
@@ -10,7 +10,7 @@ namespace Neptuo.Productivity.SolutionRunner.UI
 {
     public class VersionInfo
     {
-        internal const string Version = "1.2.0";
+        internal const string Version = "1.3.0";
 
         public static Version GetVersion()
         {
