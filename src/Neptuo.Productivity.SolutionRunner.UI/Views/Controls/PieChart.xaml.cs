ac732f7847c3af20fe036203525aaaa06d81d7f8
maraf <mara@neptuo.com>
2016-12-15 16:44:27 +0100

    Integration of PieChart.

---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/Controls/PieChart.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neptuo.Productivity.SolutionRunner.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for PieChart.xaml
+    /// </summary>
+    public partial class PieChart : ItemsControl
+    {
+        public double Thickness
+        {
+            get { return (double)GetValue(ThicknessProperty); }
+            set { SetValue(ThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThicknessProperty = DependencyProperty.Register(
+            "Thickness",
+            typeof(double),
+            typeof(PieChart),
+            new PropertyMetadata(10d, OnThicknessChanged)
+        );
+
+        private static void OnThicknessChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PieChart pieChart = (PieChart)d;
+            pieChart.Update();
+        }
+
+        public string ValuePath
+        {
+            get { return (string)GetValue(ValuePathProperty); }
+            set { SetValue(ValuePathProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValuePathProperty = DependencyProperty.Register(
+            "ValuePath",
+            typeof(string),
+            typeof(PieChart),
+            new PropertyMetadata(null)
+        );
+
+        public string ForegroundPath
+        {
+            get { return (string)GetValue(ForegroundPathProperty); }
+            set { SetValue(ForegroundPathProperty, value); }
+        }
+
+        public static readonly DependencyProperty ForegroundPathProperty = DependencyProperty.Register(
+            "ForegroundPath",
+            typeof(string),
+            typeof(PieChart),
+            new PropertyMetadata(null)
+        );
+
+        public string LabelPath
+        {
+            get { return (string)GetValue(LabelPathProperty); }
+            set { SetValue(LabelPathProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelPathProperty = DependencyProperty.Register(
+            "LabelPath", 
+            typeof(string), 
+            typeof(PieChart), 
+            new PropertyMetadata(null)
+        );
+
+        public double LabelOffset
+        {
+            get { return (double)GetValue(LabelOffsetProperty); }
+            set { SetValue(LabelOffsetProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelOffsetProperty = DependencyProperty.Register(
+            "LabelOffset",
+            typeof(double),
+            typeof(PieChart),
+            new PropertyMetadata(10d, OnLabelOffsetChanged)
+        );
+
+        private static void OnLabelOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PieChart chart = (PieChart)d;
+            chart.Update();
+        }
+
+        public PieChart()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+            Update();
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            PieChartItem item = new PieChartItem();
+
+            if (!String.IsNullOrEmpty(ValuePath))
+                item.SetBinding(PieChartItem.ValueProperty, new Binding(ValuePath));
+
+            if (!String.IsNullOrEmpty(ForegroundPath))
+                item.SetBinding(PieChartItem.ForegroundProperty, new Binding(ForegroundPath));
+
+            if (!String.IsNullOrEmpty(LabelPath))
+                item.SetBinding(PieChartItem.LabelProperty, new Binding(LabelPath));
+
+            return item;
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is PieChartItem;
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            Size result = base.ArrangeOverride(arrangeBounds);
+            Update();
+            return result;
+        }
+        
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            PieChartItem chartItem = element as PieChartItem;
+            if (chartItem != null)
+                chartItem.LabelTemplate = ItemTemplate;
+        }
+
+        internal void Update()
+        {
+            double sum = 0;
+            foreach (PieChartItem item in EnumerateItems())
+            {
+                double percentage = GetPercentage(item);
+                item.Update(sum, percentage, this);
+                sum += percentage;
+            }
+        }
+
+        private double GetPercentage(PieChartItem item)
+        {
+            double sum = EnumerateItems().Sum(i => i.Value);
+            double value = item.Value;
+
+            return (value / sum) * 100;
+        }
+
+        private IEnumerable<PieChartItem> EnumerateItems()
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                PieChartItem item = Items[i] as PieChartItem;
+                if (item != null)
+                    yield return item;
+                else
+                    yield return (PieChartItem)ItemContainerGenerator.ContainerFromIndex(i);
+            }
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Update();
+        }
+    }
+}
---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/Controls/PieChartItem.xaml.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neptuo.Productivity.SolutionRunner.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for PieChart.xaml
+    /// </summary>
+    public partial class PieChartItem : UserControl
+    {
+        public double Thickness
+        {
+            get { return (double)GetValue(ThicknessProperty); }
+            set { SetValue(ThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThicknessProperty = DependencyProperty.Register(
+            "Thickness",
+            typeof(double),
+            typeof(PieChartItem),
+            new PropertyMetadata(OnThicknessChanged)
+        );
+
+        private static void OnThicknessChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PieChartItem pc = (PieChartItem)d;
+            pc.NotifyUpdate();
+        }
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value",
+            typeof(int),
+            typeof(PieChartItem),
+            new PropertyMetadata(0, OnValueChanged)
+        );
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PieChartItem pc = (PieChartItem)d;
+            pc.NotifyUpdate();
+        }
+
+        public object Label
+        {
+            get { return GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register(
+            "Label",
+            typeof(object),
+            typeof(PieChartItem),
+            new PropertyMetadata(null, OnLabelChanged)
+        );
+
+        public DataTemplate LabelTemplate
+        {
+            get { return (DataTemplate)GetValue(LabelTemplateProperty); }
+            set { SetValue(LabelTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelTemplateProperty = DependencyProperty.Register(
+            "LabelTemplate",
+            typeof(DataTemplate),
+            typeof(PieChartItem),
+            new PropertyMetadata(null, OnLabelChanged)
+        );
+
+        public double LabelOffset
+        {
+            get { return (double)GetValue(LabelOffsetProperty); }
+            set { SetValue(LabelOffsetProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelOffsetProperty = DependencyProperty.Register(
+            "LabelOffset",
+            typeof(double),
+            typeof(PieChartItem),
+            new PropertyMetadata(10d)
+        );
+
+        private static void OnLabelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PieChartItem item = (PieChartItem)d;
+            if (item.Label != null && item.LabelTemplate != null)
+                throw new NotSupportedException("PieChartItem, set Label or LabelTemplate, not both.");
+
+            if (item.Label != null || item.LabelTemplate != null)
+                item.labelPanel.Visibility = Visibility.Visible;
+            else
+                item.labelPanel.Visibility = Visibility.Collapsed;
+
+            if (item.Label != null)
+                item.label.Content = item.Label;
+        }
+
+        public PieChartItem()
+        {
+            InitializeComponent();
+            BindEvents();
+            NotifyUpdate();
+        }
+
+        private void BindEvents()
+        {
+            DependencyPropertyDescriptor.FromProperty(ContentPresenter.ActualWidthProperty, typeof(ContentPresenter))
+                .AddValueChanged(label, OnLabelSizeChanged);
+
+            DependencyPropertyDescriptor.FromProperty(ContentPresenter.ActualHeightProperty, typeof(ContentPresenter))
+                .AddValueChanged(label, OnLabelSizeChanged);
+        }
+
+        private void OnLabelSizeChanged(object sender, EventArgs e)
+        {
+            label.Margin = new Thickness(-(label.ActualWidth / 2), -(label.ActualHeight / 2), 0, 0);
+        }
+
+        private PieChart FindParent()
+        {
+            FrameworkElement parent = FindParent(this);
+            if (parent == null)
+                return null;
+
+            PieChart chart = parent as PieChart;
+            if (chart != null)
+                return chart;
+
+            for (int i = 0; i < 20; i++)
+            {
+                parent = FindParent(parent);
+                chart = parent as PieChart;
+                if (chart != null)
+                    return chart;
+            }
+
+            return null;
+        }
+
+        private FrameworkElement FindParent(FrameworkElement element)
+        {
+            FrameworkElement parent = element.Parent as FrameworkElement;
+            if (parent == null)
+                parent = element.TemplatedParent as FrameworkElement;
+
+            if (parent == null)
+                parent = VisualTreeHelper.GetParent(element) as FrameworkElement;
+
+            return parent;
+        }
+
+        private void NotifyUpdate()
+        {
+            PieChart pieChart = FindParent();
+            if (pieChart != null)
+                pieChart.Update();
+        }
+
+        internal void Update(double offset, double percentage, PieChart pieChart)
+        {
+            double thickness = Thickness;
+            if (thickness == 0)
+                thickness = pieChart.Thickness;
+
+            double minHalfSize = Math.Min(ActualWidth, ActualHeight) / 2;
+            thickness = Math.Min(minHalfSize, thickness);
+
+            double radius = minHalfSize - thickness / 2;
+            if (radius > 0)
+            {
+                if (offset < 0)
+                    offset = 0;
+                else if (offset >= 100)
+                    offset = 99.9999d;
+
+                if (percentage < 0)
+                    percentage = 0;
+                if (percentage >= 100)
+                    percentage = 99.9999d;
+
+                double angleOffset = (offset * 360) / 100;
+                double angle = (percentage * 360) / 100;
+
+                RenderArc(radius, thickness, angleOffset, angle);
+
+                double labelOffset = GetLabelOffset(pieChart);
+                RenderLabel(radius, labelOffset, thickness, angleOffset, angle);
+            }
+        }
+
+        private void RenderArc(double radius, double thickness, double angleOffset, double angle)
+        {
+            path.StrokeThickness = thickness;
+
+            Point start = GetStartPoint(radius, angleOffset);
+            ApplyThickness(ref start, thickness);
+
+            Point end = GetEndPoint(radius, angleOffset, angle);
+            ApplyThickness(ref end, thickness);
+            NormalizePoint(ref start, ref end);
+
+            path.Width = radius * 2 + thickness;
+            path.Height = radius * 2 + thickness;
+
+            figure.StartPoint = start;
+
+            segment.Point = end;
+            segment.Size = new Size(radius, radius);
+            segment.IsLargeArc = angle > 180.0;
+        }
+
+        private void RenderLabel(double radius, double labelOffset, double thickness, double angleOffset, double angle)
+        {
+            radius += thickness / 2;
+            Point point = CartesianCoordinate(angleOffset + angle / 2, radius + labelOffset);
+            point.X += radius;
+            point.Y += radius;
+
+            label.SetValue(Canvas.LeftProperty, point.X);
+            label.SetValue(Canvas.TopProperty, point.Y);
+        }
+
+        private Point GetStartPoint(double radius, double angleOffset)
+        {
+            Point start;
+            if (angleOffset == 0)
+            {
+                start = new Point(radius, 0);
+            }
+            else
+            {
+                start = CartesianCoordinate(angleOffset, radius);
+                start.X += radius;
+                start.Y += radius;
+            }
+
+            return start;
+        }
+
+        private Point GetEndPoint(double radius, double angleOffset, double angle)
+        {
+            Point end = CartesianCoordinate(angleOffset + angle, radius);
+            end.X += radius;
+            end.Y += radius;
+
+            return end;
+        }
+
+        private void ApplyThickness(ref Point point, double thickness)
+        {
+            point.X += thickness / 2;
+            point.Y += thickness / 2;
+        }
+
+        private void NormalizePoint(ref Point start, ref Point end)
+        {
+            if (start.X == Math.Round(end.X) && start.Y == Math.Round(end.Y))
+                end.X -= 0.01;
+        }
+
+        private Point CartesianCoordinate(double angle, double radius)
+        {
+            double angleRad = (Math.PI / 180.0) * (angle - 90);
+
+            double x = radius * Math.Cos(angleRad);
+            double y = radius * Math.Sin(angleRad);
+
+            return new Point(x, y);
+        }
+
+        private double GetLabelOffset(PieChart pieChart)
+        {
+            double labelOffset = 0;
+            object rawLabelOffset = ReadLocalValue(LabelOffsetProperty);
+            if (rawLabelOffset == DependencyProperty.UnsetValue)
+                labelOffset = pieChart.LabelOffset;
+            else
+                labelOffset = (double)rawLabelOffset;
+
+            return labelOffset;
+        }
+    }
+}
