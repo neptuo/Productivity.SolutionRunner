163c68d136c0936df20e1f8577c9109734e05b26
maraf <mara@neptuo.com>
2017-01-02 17:05:46 +0100

    Removing default value for FileSearchPattern which was accidentally set to False.

--- src/Neptuo.Productivity.SolutionRunner.UI/Properties/Settings.Designer.cs
+++ src/Neptuo.Productivity.SolutionRunner.UI/Properties/Settings.Designer.cs
@@ -97,7 +97,7 @@ namespace Neptuo.Productivity.SolutionRunner.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string FileSearchPattern {
             get {
                 return ((string)(this["FileSearchPattern"]));
