3a26d4a9d3c996648ff4dbef519d979c0f4d394e
Marek Fišera <mara@neptuo.com>
2017-04-19 20:18:01 +0200

    Add MarkupExtensionReturnType. Finding target object.

--- src/Neptuo.Productivity.SolutionRunner.UI/Views/Themes/Color.cs
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/Themes/Color.cs
@@ -5,16 +5,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Markup;
+using System.Windows.Media;
 
 namespace Neptuo.Productivity.SolutionRunner.Views.Themes
 {
-    public class Color : MarkupExtension
+    [MarkupExtensionReturnType(typeof(System.Windows.Media.Color))]
+    public class Color : MarkupExtension, IProvider
     {
+        private static List<Color> items = new List<Color>();
+
+        public static void UpdateAll()
+        {
+            foreach (Color item in items)
+                item.Update();
+        }
+
+        private SolidColorBrush targetObject;
+        private DependencyProperty targetProperty;
+
         public System.Windows.Media.Color Dark { get; set; }
         public System.Windows.Media.Color Light { get; set; }
 
+        public void Update()
+        {
+            if (targetObject == null || targetProperty == null)
+                return;
+
+            targetObject.SetValue(targetProperty, GetValue());
+        }
+
         public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            IXamlTypeResolver xamlTypeResolver = (IXamlTypeResolver)serviceProvider.GetService(typeof(IXamlTypeResolver));
+            IProvideValueTarget provideValueTarget = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+
+            targetObject = provideValueTarget.TargetObject as SolidColorBrush;
+            targetProperty = provideValueTarget.TargetProperty as DependencyProperty;
+            items.Add(this);
+
+            return GetValue();
+        }
+
+        private object GetValue()
         {
             switch (Settings.Default.ThemeMode)
             {
