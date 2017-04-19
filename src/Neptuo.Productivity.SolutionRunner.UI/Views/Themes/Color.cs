7a68059c2d28000ca846691167686943acf27c3f
Marek Fišera <mara@neptuo.com>
2017-04-19 19:15:02 +0200

    Add MarkupExtension for switching color based on theme.

---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/Themes/Color.cs
@@ -0,0 +1,30 @@
+﻿using Neptuo.Productivity.SolutionRunner.Properties;
+using Neptuo.Productivity.SolutionRunner.Services.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Neptuo.Productivity.SolutionRunner.Views.Themes
+{
+    public class Color : MarkupExtension
+    {
+        public System.Windows.Media.Color Dark { get; set; }
+        public System.Windows.Media.Color Light { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            switch (Settings.Default.ThemeMode)
+            {
+                case ThemeMode.Dark:
+                    return Dark;
+                case ThemeMode.Light:
+                    return Light;
+                default:
+                    return null;
+            }
+        }
+    }
+}
