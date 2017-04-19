e22311748a0d19657193101b53dd493edf807cc4
Marek Fišera <mara@neptuo.com>
2017-04-19 19:01:17 +0200

    Add settings for switching themes.

--- src/Neptuo.Productivity.SolutionRunner.UI/Properties/Settings.Designer.cs
+++ src/Neptuo.Productivity.SolutionRunner.UI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Neptuo.Productivity.SolutionRunner.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -284,6 +284,18 @@ namespace Neptuo.Productivity.SolutionRunner.Properties {
             }
             set {
                 this["AutoSelectApplicationMinimalVersion"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Dark")]
+        public global::Neptuo.Productivity.SolutionRunner.Services.Themes.ThemeMode ThemeMode {
+            get {
+                return ((global::Neptuo.Productivity.SolutionRunner.Services.Themes.ThemeMode)(this["ThemeMode"]));
+            }
+            set {
+                this["ThemeMode"] = value;
             }
         }
     }
