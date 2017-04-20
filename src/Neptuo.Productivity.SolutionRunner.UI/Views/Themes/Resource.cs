ea090695d089a17662193a6e559c6e2c2098821c
Marek Fišera <mara@neptuo.com>
2017-04-20 20:39:29 +0200

    Move theme resources out for App.Resources, use enum and switch.

---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/Themes/Resource.cs
@@ -0,0 +1,50 @@
+﻿using Neptuo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Neptuo.Productivity.SolutionRunner.Views.Themes
+{
+    public class Resource : MarkupExtension
+    {
+        [ConstructorArgument("Type")]
+        public ResourceType Type { get; set; }
+
+        public Resource(ResourceType type)
+        {
+            Type = type;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            switch (Type)
+            {
+                case ResourceType.BackgroundBrush:
+                    break;
+                case ResourceType.ForegroundBrush:
+                    break;
+                case ResourceType.ActiveColor:
+                    break;
+                case ResourceType.InactiveColor:
+                    return System.Windows.Media.Color.FromRgb(63, 63, 70);
+                case ResourceType.ActiveBrush:
+                    break;
+                case ResourceType.InactiveBrush:
+                    break;
+                case ResourceType.HoverBrush:
+                    break;
+                case ResourceType.TextBoxInactiveBrush:
+                    break;
+                case ResourceType.GrayBrush:
+                    break;
+                case ResourceType.LinkForegroundBrush:
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/Themes/ResourceType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.SolutionRunner.Views.Themes
+{
+    public enum ResourceType
+    {
+        BackgroundBrush,
+        ForegroundBrush,
+
+        ActiveColor,
+        InactiveColor,
+
+        ActiveBrush,
+        InactiveBrush,
+        HoverBrush,
+        TextBoxInactiveBrush,
+        GrayBrush,
+        LinkForegroundBrush
+    }
+}
