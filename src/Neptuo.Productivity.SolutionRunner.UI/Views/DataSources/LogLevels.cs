de36dd4129a64ae0abf8eaf6d8f61ff1cb15146f
Marek Fišera <mara@neptuo.com>
2018-03-15 18:47:59 +0100

    UI for min log level.

---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/DataSources/LogLevels.cs
@@ -0,0 +1,25 @@
+﻿using Neptuo.Logging;
+using Neptuo.Productivity.SolutionRunner.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Neptuo.Productivity.SolutionRunner.Views.DataSources
+{
+    public class LogLevels : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new List<NameValueViewModel>()
+            {
+                new NameValueViewModel("Debug", LogLevel.Debug),
+                new NameValueViewModel("Information", LogLevel.Info),
+                new NameValueViewModel("Warning", LogLevel.Warning),
+                new NameValueViewModel("Error", LogLevel.Error)
+            };
+        }
+    }
+}
