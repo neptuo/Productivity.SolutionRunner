806b7ee09370218fc414a850492558b31d0da642
Marek Fišera <mara@neptuo.com>
2019-02-10 08:12:08 +0100

    #88 - Quick and dirty solution for removing access key border when not configured.

---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Views/Converters/KeyToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Neptuo.Productivity.SolutionRunner.Views.Converters
+{
+    public class KeyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Key? key = value as Key?;
+            if (key == null || key.Value == Key.None)
+                return Visibility.Collapsed;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
