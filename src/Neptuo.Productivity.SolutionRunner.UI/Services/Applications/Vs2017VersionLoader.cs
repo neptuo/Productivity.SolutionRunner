d74e06191baa5742901ea73b1dc5261920b62611
Marek Fišera <mara@neptuo.com>
2020-11-12 17:23:58 +0100

    #96 - Add command for starting VS updater.

--- src/Neptuo.Productivity.SolutionRunner.UI/Services/Applications/Vs2017VersionLoader.cs
+++ src/Neptuo.Productivity.SolutionRunner.UI/Services/Applications/Vs2017VersionLoader.cs
@@ -14,7 +14,7 @@ namespace Neptuo.Productivity.SolutionRunner.Services.Applications
     {
         public void Add(IApplicationCollection applications)
         {
-            foreach (var (name, filePath, version) in Enumerate().OrderBy(vs => vs.name))
+            foreach (var (name, installationPath, filePath, version) in Enumerate().OrderBy(vs => vs.name))
             {
                 IApplicationBuilder builder = applications.Add(
                     name,
@@ -30,10 +30,11 @@ namespace Neptuo.Productivity.SolutionRunner.Services.Applications
 
                 VsVersionLoader.AddAdministratorCommand(builder, filePath);
                 VsVersionLoader.AddExperimentalCommand(builder, filePath);
+                VsInstallerCommandLoader.AddCommand(builder, installationPath);
             }
         }
 
-        private IEnumerable<(string name, string filePath, Version version)> Enumerate()
+        private IEnumerable<(string name, string installationPath, string filePath, Version version)> Enumerate()
         {
             var query = new SetupConfiguration();
             var query2 = (ISetupConfiguration2)query;
@@ -55,10 +56,11 @@ namespace Neptuo.Productivity.SolutionRunner.Services.Applications
             while (fetched > 0);
         }
 
-        private bool TryGetInstance(ISetupInstance instance, ISetupHelper helper, out (string name, string filePath, Version version) output)
+        private bool TryGetInstance(ISetupInstance instance, ISetupHelper helper, out (string name, string installationPath, string filePath, Version version) output)
         {
             string name = "Visual Studio";
             string filePath = null;
+            string installationPath = null;
             Version version = null;
 
             var instance2 = (ISetupInstance2)instance;
@@ -69,7 +71,10 @@ namespace Neptuo.Productivity.SolutionRunner.Services.Applications
                 Version.TryParse(instance.GetInstallationVersion(), out version);
 
                 if ((state & InstanceState.Local) == InstanceState.Local)
-                    filePath = Path.Combine(instance2.GetInstallationPath(), @"Common7\IDE\devenv.exe");
+                {
+                    installationPath = instance2.GetInstallationPath();
+                    filePath = Path.Combine(installationPath, @"Common7\IDE\devenv.exe");
+                }
 
                 if (instance is ISetupInstanceCatalog catalog)
                 {
@@ -92,16 +97,16 @@ namespace Neptuo.Productivity.SolutionRunner.Services.Applications
             if (name != null && filePath != null && File.Exists(filePath) && version != null)
             {
                 name = String.Format(
-                    "{0} {1}", 
-                    name, 
+                    "{0} {1}",
+                    name,
                     VersionFormatter.Format(version)
                 );
 
-                output = (name, filePath, version);
+                output = (name, installationPath, filePath, version);
                 return true;
             }
 
-            output = default((string, string, Version));
+            output = default((string, string, string, Version));
             return false;
         }
 
---
+++ src/Neptuo.Productivity.SolutionRunner.UI/Services/Applications/VsInstallerCommandLoader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Neptuo.Productivity.SolutionRunner.Services.Applications
+{
+    public static class VsInstallerCommandLoader
+    {
+        private static bool? hasInstaller;
+        private static string installerPath;
+
+        public static void AddCommand(IApplicationBuilder application, string installationPath)
+        {
+            if (EnsureInstallerPath())
+            {
+                string args = $"-- modify --installPath \"{installationPath}\"";
+                application.AddCommand("Installer", installerPath, args, args, true, Key.I);
+            }
+        }
+
+        private static bool EnsureInstallerPath()
+        {
+            if (hasInstaller == null)
+            {
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Microsoft Visual Studio", "Installer", "vs_installer.exe");
+                if (File.Exists(path))
+                {
+                    hasInstaller = true;
+                    installerPath = path;
+                }
+                else
+                {
+                    hasInstaller = false;
+                }
+            }
+
+            return hasInstaller.Value;
+        }
+    }
+}
