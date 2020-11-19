2c253d629e9f20285e658e3b6eb8010c331a8429
Marek Fišera <mara@neptuo.com>
2020-11-19 22:10:48 +0100

    #101 - Using mutex and broadcast windows message to allow only a single instance of application.

--- src/Neptuo.Productivity.SolutionRunner.UI/AppEntryPoint.cs
+++ src/Neptuo.Productivity.SolutionRunner.UI/AppEntryPoint.cs
@@ -1,18 +1,40 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace Neptuo.Productivity.SolutionRunner
-//{
-//    public class AppEntryPoint
-//    {
-//        [STAThread]
-//        public static void Main(string[] args)
-//        {
-//            AppEntryPointManager manager = new AppEntryPointManager();
-//            manager.Run(args);
-//        }
-//    }
-//}
+namespace Neptuo.Productivity.SolutionRunner
+{
+    public class AppEntryPoint : AppSingleInstace
+    {
+        [STAThread]
+        public static void Main(string[] args) => new AppEntryPoint().Run();
+
+        public AppEntryPoint()
+        {
+#if DEBUG
+            ApplicationId = new Guid("54d7d4aa-db22-4582-86c6-a70badbed65a");
+#else
+            ApplicationId = new Guid("8a8ec8b3-8da7-400a-864b-c992c3f66f31");
+#endif
+        }
+
+        private App app;
+
+        protected override void OnStartup()
+        {
+            app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        protected override void OnStartupNextInstance()
+        {
+            app.Activate();
+        }
+    }
+}
---
+++ src/Neptuo.Productivity.SolutionRunner.UI/AppSingleInstace.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace Neptuo.Productivity.SolutionRunner
+{
+    public abstract class AppSingleInstace
+    {
+        private static void NotifyOther()
+            => Win32.PostMessage((IntPtr)Win32.HwndBroadcast, Win32.WmActivate, IntPtr.Zero, IntPtr.Zero);
+
+        public void Run()
+        {
+            using (Mutex mutex = new Mutex(true, ApplicationId.ToString(), out var isNew))
+            {
+                if (isNew)
+                {
+                    ComponentDispatcher.ThreadFilterMessage += ComponentDispatcher_ThreadFilterMessage;
+                    OnStartup();
+                }
+                else
+                {
+                    NotifyOther();
+                }
+            }
+        }
+        
+        private void ComponentDispatcher_ThreadFilterMessage(ref MSG msg, ref bool handled)
+        {
+            if (msg.message == Win32.WmActivate)
+                OnStartupNextInstance();
+        }
+
+        protected Guid ApplicationId { get; set; }
+
+        protected abstract void OnStartup();
+        protected abstract void OnStartupNextInstance();
+
+        internal class Win32
+        {
+            public static readonly IntPtr HwndBroadcast = (IntPtr)0xffff;
+            public static readonly int WmActivate = RegisterWindowMessage("WM_SOLUTIONRUNNER_ACTIVATE");
+
+            [DllImport("user32")]
+            public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+
+            [DllImport("user32")]
+            public static extern int RegisterWindowMessage(string message);
+        }
+    }
+}
